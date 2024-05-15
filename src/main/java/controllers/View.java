6889efdfda2eb7f927a621bd48c1e748b8a53476
Besarta Mustafa <besartamustafa49@gmail.com>
2024-05-15 22:17:29 +0200

    view file

---
+++ src/main/java/controllers/View.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import app.Navigator;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class View implements Initializable {
+    @FXML
+    private Label Exit;
+    @FXML
+    private Label Menu;
+
+    @FXML
+    private Label MenuBack;
+
+    @FXML
+    private AnchorPane slider;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Exit.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+        slider.setTranslateX(-176);
+        Menu.setOnMouseClicked(event -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(slider);
+
+            slide.setToX(0);
+            slide.play();
+
+            slider.setTranslateX(-176);
+
+            slide.setOnFinished((ActionEvent e)-> {
+                Menu.setVisible(false);
+                MenuBack.setVisible(true);
+            });
+        });
+
+        MenuBack.setOnMouseClicked(event -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(slider);
+
+            slide.setToX(-176);
+            slide.play();
+
+            slider.setTranslateX(0);
+
+            slide.setOnFinished((ActionEvent e)-> {
+                Menu.setVisible(true);
+                MenuBack.setVisible(false);
+            });
+        });
+    }
+    @FXML
+    void HandleLogin(MouseEvent event) {
+        Navigator.navigate(event, Navigator.LOGIN_PAGE);
+    }
+}
+
