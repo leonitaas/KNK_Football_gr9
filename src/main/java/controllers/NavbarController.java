8d70de0274a7768bb07a5284824c350bbc6606e6
ereza4 <erezatemaj@gmail.com>
2024-05-16 21:04:05 +0200

    Navbari

---
+++ src/main/java/controllers/NavbarController.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class NavbarController {
+
+    @FXML
+    private Button homebutton;
+    @FXML
+    private Button matchesButton;
+
+    @FXML
+    private Button teamsButton;
+
+    @FXML
+    private Button playersButton;
+
+    @FXML
+    private Button leaguesButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+
+}
+
