abb7afdd6115e844f5668540d14d1406ac47083c
ereza4 <erezatemaj@gmail.com>
2024-05-24 19:59:34 +0200

    update

--- src/main/java/controllers/Home.java
+++ src/main/java/controllers/Home.java
@@ -14,60 +14,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Home implements Initializable {
-    @FXML
-    private Label Exit;
-    @FXML
-    private Label Menu;
 
-    @FXML
-    private Label MenuBack;
 
-    @FXML
-    private AnchorPane slider;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        Exit.setOnMouseClicked(event -> {
-            System.exit(0);
-        });
-        slider.setTranslateX(-260);
-        Menu.setOnMouseClicked(event -> {
-            TranslateTransition slide = new TranslateTransition();
-            slide.setDuration(Duration.seconds(0.4));
-            slide.setNode(slider);
 
-            slide.setToX(0);
-            slide.play();
-
-            slider.setTranslateX(-260);
-
-            slide.setOnFinished((ActionEvent e) -> {
-                Menu.setVisible(false);
-                MenuBack.setVisible(true);
-            });
-        });
-
-        MenuBack.setOnMouseClicked(event -> {
-            TranslateTransition slide = new TranslateTransition();
-            slide.setDuration(Duration.seconds(0.4));
-            slide.setNode(slider);
-
-            slide.setToX(-260);
-            slide.play();
-
-            slider.setTranslateX(0);
-
-            slide.setOnFinished((ActionEvent e) -> {
-                Menu.setVisible(true);
-                MenuBack.setVisible(false);
-            });
-        });
-    }
-
-    @FXML
-    void HandleLogin(MouseEvent event) {
-        Navigator.navigate(event, Navigator.LOGIN_PAGE);
     }
 
 }
